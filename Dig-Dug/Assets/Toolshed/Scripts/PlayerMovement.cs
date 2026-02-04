09e9c46e928b0414c0d0000de11025f7fc3e732e
tiptonblu3 <tiptonblu3@gmail.com>
2026-02-04 13:30:19 -0500

    Simple Player Movement

--- Dig-Dug/Assets/Toolshed/Scripts/PlayerMovement.cs
+++ Dig-Dug/Assets/Toolshed/Scripts/PlayerMovement.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //declare variables
+    public float MoveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Vector2 MoveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Grabs Rigidbody2D from Player
+       rb =  GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move Calcs
+        rb.linearVelocity = MoveInput * MoveSpeed;
+    }
+
+    //Movement
+    public void Move(InputAction.CallbackContext context)
+    {
+        MoveInput = context.ReadValue<Vector2>();
     }
 }
