d65a72b4a43184741740c715639e1659e3cb2846
Snadicus <iscreamatrats@gmail.com>
2026-02-07 05:04:42 -0500

    Updated Enemy Behavior to start testing collision

--- Dig-Dug/Assets/Toolshed/Scripts/Enemies/EnemyBehavior.cs
+++ Dig-Dug/Assets/Toolshed/Scripts/Enemies/EnemyBehavior.cs
@@ -32,7 +32,7 @@ public class EnemyBehavior : MonoBehaviour
     void Update()
     {
         // Records player location for testing
-        Debug.Log("Player Position: X = " + PlayerObj.transform.position.x + " --- Y = " + PlayerObj.transform.position.y);
+        //Debug.Log("Player Position: X = " + PlayerObj.transform.position.x + " --- Y = " + PlayerObj.transform.position.y);
 
         // Moves enemy constantly towards Player position
         PlayerXPosition = PlayerObj.transform.position.x;
@@ -40,6 +40,38 @@ public class EnemyBehavior : MonoBehaviour
         Target = new Vector2(PlayerXPosition, PlayerYPosition);
         Step = MoveSpeed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, Target, Step);
+    }
+
+
+    // Collision Test Code
+    void OnCollisionEnter2D(Collision2D Collision)
+    {
+        if (Collision.gameObject.name == "Lv1 Dirt Top")
+        {
+            Debug.Log("Touching Lv1 Dirt Top");
+            if (PlayerYPosition >= 0)
+            {
+                //transform.position = Vector2.MoveTowards(transform.position, Target, Step);
+                Debug.Log("Above");
+            } 
+            else if (PlayerYPosition < 0)
+            {
+                Debug.Log("Below");
+            }
+        } 
+        else if (Collision.gameObject.name == "Lv1 Dirt")
+        {
+            Debug.Log("Touching Lv1 Dirt");
+        } 
+        else if (Collision.gameObject.name == "Lv2 Dirt")
+        {
+            Debug.Log("Touching Lv2 Dirt");
+        }
+        else if (Collision.gameObject.name == "Lv3 Dirt")
+        {
+            Debug.Log("Touching Lv3 Dirt");
+        }
 
     }
+
 }
